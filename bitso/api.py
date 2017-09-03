3355b0141feb197ed38ebb649b1bdb1c6707a8a4
baruc <baruc.almaguer@gmail.com>
2017-09-03 10:28:49 -0500

    corregidos imports de api.py (py2->py3)

--- bitso/api.py
+++ bitso/api.py
@@ -30,9 +30,10 @@ import hmac
 import json
 import time
 import requests
-from urlparse import urlparse
-from urllib import urlencode
-
+#from urlparse import urlparse
+from urllib.parse import urlparse
+#from urllib import urlencode
+from urllib.parse import urlencode
 
 from bitso import (ApiError, ApiClientError, Ticker, OrderBook, Balances, Fees, Trade, UserTrade, Order, TransactionQuote, TransactionOrder, LedgerEntry, FundingDestination, Withdrawal, Funding, AvailableBooks, AccountStatus, AccountRequiredField)
 
